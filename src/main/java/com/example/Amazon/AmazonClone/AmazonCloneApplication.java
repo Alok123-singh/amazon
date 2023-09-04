86973e50a864631b3e1acf7b7cdf0321e8dd7407
Alok Singh Bais <74749762+Alok123-singh@users.noreply.github.com>
2023-09-04 12:10:01 +0530

    Update AmazonCloneApplication.java

--- src/main/java/com/example/Amazon/AmazonClone/AmazonCloneApplication.java
+++ src/main/java/com/example/Amazon/AmazonClone/AmazonCloneApplication.java
@@ -16,6 +16,7 @@ public class AmazonCloneApplication {
 	public static void main(String[] args) {
 //
 		SpringApplication.run(AmazonCloneApplication.class, args);
+		//hdhcyjcjcjcjcj,ycyjcj
 	}
 
 
