2d552989c21035a9aa94ca8a80c1b8d7247989e3
alok123-singh <aloksinghbais02@gmail.com>
2023-09-04 12:08:04 +0530

    test push

--- src/main/java/com/example/Amazon/AmazonClone/AmazonCloneApplication.java
+++ src/main/java/com/example/Amazon/AmazonClone/AmazonCloneApplication.java
@@ -14,6 +14,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class AmazonCloneApplication {
 
 	public static void main(String[] args) {
+//
 		SpringApplication.run(AmazonCloneApplication.class, args);
 	}
 
