c1b68b1e2e7e9fddcc432e7663ec4e1a314e1deb
alok123-singh <aloksinghbais02@gmail.com>
2023-09-04 12:43:39 +0530

    Updating Test

--- src/main/java/com/example/Amazon/AmazonClone/AmazonCloneApplication.java
+++ src/main/java/com/example/Amazon/AmazonClone/AmazonCloneApplication.java
@@ -16,8 +16,6 @@ public class AmazonCloneApplication {
 	public static void main(String[] args) {
 //
 		SpringApplication.run(AmazonCloneApplication.class, args);
-
-		System.out.println("Hello World!");
 	}
 
 
